604c96ee49a9e54fc05e45b63c16d204f670e4b3
Daria <zaikinad@gmail.com>
2018-11-25 13:12:49 +0700

    update2

--- app/js/events.js
+++ app/js/events.js
@@ -1,66 +1,95 @@
-applications = [{
-		image: 'assets/shot-1.png',
-		head: 'СТАНДАРТНЫЙ ПАКЕТ',
-		date: '08 впреля 2012'
-	}, {
-		image: 'assets/shot-2.png',
-		head: 'НОВЫЙ ЦФТ-БАНК',
-		date: '09 сентября 2016'
-	}, {
-		image: 'assets/shot-3.png',
-		head: 'КАТАЛОГ РАЗРАБОТОК',
-		date: '03 марта 2015'
-	}, {
-		image: 'assets/shot-3.png',
-		head: 'КАТАЛОГ РАЗРАБОТОКK',
-		date: '03 марта 2015'
-	}, {
-		image: 'assets/shot-3.png',
-		head: 'КАТАЛОГ РАЗРАБОТОКKK',
-		date: '03 марта 2015'
-	}, {
-		image: 'assets/shot-3.png',
-		head: 'КАТАЛОГ РАЗРАБОТОКKKK',
-		date: '03 марта 2015'
-	}
-];
-var i = 2 ;
-function createApplication() {
+var xhr = new XMLHttpRequest();
+xhr.open("GET","http://localhost:3000/api/APP.json", true);
+xhr.send();
 
-let imageBlockDiv = document.getElementsByClassName('info-images')[0];
-
-let imageBlock = document.createElement('div');
-imageBlock.className = 'image-block clearfix';
-
-
-
-imageBlockDiv.appendChild(imageBlock);
-
-let imageElement = document.createElement('img');
-imageElement.className = 'image';
-imageElement.src = applications[i].image;
-
-imageBlock.appendChild(imageElement);
-
-let spanElement = document.createElement('span');
-spanElement.className = 'text-under-image heading-level-4';
-spanElement.innerHTML = applications[i].head;
-
-imageBlock.appendChild(spanElement);
-
-let span2Element = document.createElement('span');
-span2Element.className = 'date-under-text';
-span2Element.innerHTML = applications[i].date;
-
-imageBlock.appendChild(span2Element);
+xhr.onreadystatechange = function () {
+if (xhr.status == 200 && xhr.readyState == 4) {
+    console.log("xhr.responseText", xhr.responseText);
+    appJSON = JSON.parse(xhr.responseText);
+    console.log(appJSON.length);
+    createAllApplications(appJSON);
+	console.log(appJSON[0].image);
+	step = appJSON.length - 3;
+ }
 }
 
+let imageSwitchRight = document.querySelector('.image-switch-right');
+	imageSwitchRight.addEventListener('click', switchRight); 
+
+let imageSwitchLeft = document.querySelector('.image-switch-left');
+	imageSwitchLeft.addEventListener('click',switchLeft); 
+
+
+function createApplication(application) {
+let imageBlockDiv = document.getElementsByClassName('images-line clearfix')[0];
+
+let imageBlock = document.createElement('li');
+	imageBlock.className = 'image-block';
+	
+let imageBlockContainer = document.createElement('div');
+	imageBlockContainer.className = 'image-block__container';
+	
+let imageBlockImg = document.createElement('img');
+	imageBlockImg.className = 'image-block__img';
+	imageBlockImg.src = application.image;
+console.log(application.image);
+	
+let textUnderImage = document.createElement('div');
+	textUnderImage.className = 'text-under-image';
+	textUnderImage.innerHTML = application.title;
+
+let dateUnderText = document.createElement('div');
+	dateUnderText.className = 'date-under-text';
+	dateUnderText.innerHTML = application.lastUpdate;
+
+
+	imageBlockDiv.appendChild(imageBlock);
+	imageBlock.appendChild(imageBlockContainer);
+	imageBlockContainer.appendChild(imageBlockImg);
+	imageBlockContainer.appendChild(textUnderImage);
+	imageBlockContainer.appendChild(dateUnderText);
+};
+
+function createAllApplications (applications) {
+var i = 0;
 do {
-    createApplication ();
-    i ++;
+    createApplication(applications[i]);
+	i ++;
 }
-while (i < 5);
+while (i < applications.length);
+};
 
-function changeApplicationsToRight (i) {
-     return i ++;
+var position = 1;
+function switchRight () {
+	if (position = 1) {
+		let imageBlock = document.querySelector('.images-line');
+		let switchLength = step * 363;
+		imageBlock.style.marginLeft = -switchLength + 'px';
+		position --;
+		console.log("step",step);
+	};
+	if (position < step && position > 1) {
+		let imageBlock = document.querySelector('.images-line');
+		let switchLength = position * 363;
+		imageBlock.style.marginLeft = switchLength + 'px';
+		position --;
+		console.log("step",step);
+	};
+}
+
+function switchLeft () {
+	if (position >= step) {
+	let imageBlock = document.querySelector('.images-line');
+	let switchLength =  0;
+	imageBlock.style.marginLeft = switchLength + 'px';
+	position ++;
+	console.log("step",step);
+	};
+	if (position < step) {
+	let imageBlock = document.querySelector('.images-line');
+	let switchLength =  -position * 363;
+	imageBlock.style.marginLeft = switchLength + 'px';
+	position ++;
+	console.log("step",step);
+	};
 }
